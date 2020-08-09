0100aa7704ea1823bea69ddc32b972484dcb8d8e
coxla1 <69416826+coxla1@users.noreply.github.com>
2020-08-09 23:03:38 +0200

    Use PyZ3R lib instead of Selenium

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-# select-random-sprite
-Using Selenium and Chrome, select a random sprite from a restricted list. 
+# random-sprite
+Using [PyZ3R library](https://github.com/tcprescott/pyz3r), select a random sprite from a restricted list. 
 
 # Install instructions
 * Clone or download this repository.
-* Install [Python 3.x](https://www.python.org/), _don't forget to add to PATH variable when installing_.
-* Install [Google Chrome](https://www.google.com/intl/en/chrome/).
-* Finally download the [chromedriver](https://sites.google.com/a/chromium.org/chromedriver/downloads) corresponding to your version (most liley the latest) and unzip it in the current directory.
+* Install [Python 3.6+](https://www.python.org/), _don't forget to add to PATH variable when installing_.
 * In a command line, install the requirements using : `python -m pip install -r requirements.txt`
 * Rename `default.yaml` to `config.yaml`
 * Copy the JAP 1.0 ROM of ALTTP in the current directory (google for it) with the following name (this can also be edited in `config.yaml`) : `Zelda no Densetsu - Kamigami no Triforce (Japan).sfc`
 
 # Usage
-* Configure your settings by editing config.yaml file (options are the same available on the website)
+* Configure your settings by editing config.yaml file (options are the ones available on the website)
 * Either execute `run.bat` (Windows only) or do the following from command line : `python main.py`
-* Paste seed URL, wait for it to download (Do not try to interfere with the page)
+* Paste seed URL, wait for it to create a patched file in `output/`
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-
 import numpy.random as rd
-import time
 
 import os
+import pyz3r
+import asyncio
 
 from yaml import load
 try:
@@ -13,22 +10,13 @@ try:
 except ImportError:
     from yaml import Loader
 
-def set_input(driver, ipt):
-    driver.switch_to.active_element.send_keys(ipt)
-    driver.switch_to.active_element.send_keys(Keys.RETURN)
-    driver.switch_to.active_element.send_keys(Keys.TAB)
-    time.sleep(0.1)
-
-def set_checkbox(driver):
-    driver.switch_to.active_element.send_keys(Keys.SPACE)
-    driver.switch_to.active_element.send_keys(Keys.TAB)
-    time.sleep(0.1)
-
 def choose_setting(weights):
     l = []
     for x in weights:
         for _ in range(weights[x]):
             l.append(x)
+    if len(l) == 0:
+        l = list(weights.keys())
     return l[rd.randint(0,len(l))]
 
 # Settings (loaded from config.yaml)
@@ -36,65 +24,44 @@ def choose_setting(weights):
 settings = load(open('config.yaml'), Loader=Loader)
 settings['heart_speed']['off'] = settings['heart_speed'].pop(False)
 
-rom_path = settings['rompath']
+if os.name == 'nt':
+    rom_path = os.getcwd() + '\\' + settings['rompath']
+else:
+    rom_path = os.getcwd() + '/' + settings['rompath']
+
 heart_speed = choose_setting(settings['heart_speed'])
 menu_speed = choose_setting(settings['menu_speed'])
 sprite = choose_setting(settings['sprite'])
 heart_color = choose_setting(settings['heart_color'])
 background_music = choose_setting(settings['background_music'])
 quickswap = choose_setting(settings['quickswap'])
-palette_shuffle = choose_setting(settings['palette_shuffle'])
 
-# Setup Driver
+hash = input('Please paste seed URL or hash here : ')
+if '/' in hash:
+    tmp = hash[::-1]
+    hash = tmp[:tmp.find('/')][::-1]
 
-# seed_url = 'https://alttpr.com/en/h/E4G6gQpXMZ'
-seed_url = input('Please paste seed URL here : ')
+async def random_sprite():
+    base_rom = await pyz3r.rom.read(rom_path)
+    seed = await pyz3r.alttpr(hash_id=hash)
 
-opt = webdriver.ChromeOptions()
-opt.add_argument('--start-maximized')
-driver = webdriver.Chrome(executable_path='chromedriver.exe', options=opt)
+    patched_rom = await seed.create_patched_game(
+        base_rom,
+        heartspeed=heart_speed,
+        heartcolor=heart_color,
+        spritename=sprite,
+        music=background_music,
+        quickswap=quickswap,
+        menu_speed=menu_speed
+    )
 
-# Upload Z3 ROM
+    await pyz3r.rom.write(patched_rom, 'output/' + hash + '.sfc')
 
-driver.get(seed_url)
-driver.find_element(By.TAG_NAME, 'input').send_keys(os.getcwd() + '\\' + rom_path)
-time.sleep(5)
+    if os.name == 'nt':
+        print('File written to ' + os.getcwd() + '\\output\\' + hash + '.sfc')
+    else:
+        print('File written to ' + os.getcwd() + '/output/' + hash + '.sfc')
 
-# Is it a Race ROM ?
-
-race_rom = 'bg-info' in driver.find_element(By.XPATH, "//div[@id='seed-details']/div[1]").get_attribute('class')
-
-# Skip things : navbar, ads and buttons
-
-driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.TAB)
-while 'nav' in driver.switch_to.active_element.get_attribute('class'): # Navbar
-    driver.switch_to.active_element.send_keys(Keys.TAB)
-    time.sleep(0.1)
-while driver.switch_to.active_element.get_attribute('name') != '': # Ads
-    driver.switch_to.active_element.send_keys(Keys.TAB)
-    time.sleep(0.1)
-for _ in range(2): # Buttons
-    driver.switch_to.active_element.send_keys(Keys.TAB)
-    time.sleep(0.1)
-
-# Apply settings
-
-set_input(driver,heart_speed)
-set_input(driver,sprite)
-if not race_rom:
-    set_input(driver,menu_speed)
-set_input(driver,heart_color)
-
-if driver.switch_to.active_element.is_selected() != background_music:
-    set_checkbox(driver)
-if not race_rom and driver.switch_to.active_element.is_selected() != quickswap:
-    set_checkbox(driver)
-if driver.switch_to.active_element.is_selected() != palette_shuffle:
-    set_checkbox(driver)
-
-# Download the seed and close browser
-
-time.sleep(1)
-driver.find_element(By.CLASS_NAME, 'btn-success').click()
-time.sleep(5)
-driver.close()
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(random_sprite())
