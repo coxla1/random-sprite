560d597fb92bb516f64fa4e12e79c337db73facd
Nicolas Sarkis <nico.sarkis@gmail.com>
2020-09-23 17:38:03 +0200

    MSU

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # random-sprite
-Using [PyZ3R library](https://github.com/tcprescott/pyz3r), select a random sprite from a restricted list. 
+Using [PyZ3R library](https://github.com/tcprescott/pyz3r), select a random sprite from a restricted list, apply a MSU pack (if available) and then launch the seed.
 
 # Install instructions
 * Clone or download this repository.
@@ -7,8 +7,10 @@ Using [PyZ3R library](https://github.com/tcprescott/pyz3r), select a random spri
 * In a command line, install the requirements using : `python -m pip install -r requirements.txt`
 * Rename `default.yaml` to `config.yaml`
 * Copy the JAP 1.0 ROM of ALTTP in the current directory (google for it) with the following name (this can also be edited in `config.yaml`) : `Zelda no Densetsu - Kamigami no Triforce (Japan).sfc`
+* If you want to automatically start the seed when generated : associate `.sfc` file extension with your emulator.
 
 # Usage
 * Configure your settings by editing `config.yaml` file (options are the ones available on the website)
 * Either execute `run.bat` (Windows only) or do the following from command line : `python main.py`
 * Paste seed URL, wait for it to create a patched file in `output/`
+* Note : if you automatically start the seed, the command line will not close, you can do it when emulator has started.
--- main.py
+++ main.py
@@ -13,35 +13,13 @@ except ImportError:
 def choose_setting(weights):
     l = []
     for x in weights:
-        for _ in range(weights[x]):
+        for _ in range((weights[x])):
             l.append(x)
     if len(l) == 0:
         l = list(weights.keys())
     return l[rd.randint(0,len(l))]
 
-# Settings (loaded from config.yaml)
-
-settings = load(open('config.yaml'), Loader=Loader)
-settings['heart_speed']['off'] = settings['heart_speed'].pop(False)
-
-if os.name == 'nt':
-    rom_path = os.getcwd() + '\\' + settings['rompath']
-else:
-    rom_path = os.getcwd() + '/' + settings['rompath']
-
-heart_speed = choose_setting(settings['heart_speed'])
-menu_speed = choose_setting(settings['menu_speed'])
-sprite = choose_setting(settings['sprite'])
-heart_color = choose_setting(settings['heart_color'])
-background_music = choose_setting(settings['background_music'])
-quickswap = choose_setting(settings['quickswap'])
-
-hash = input('Please paste seed URL or hash here : ')
-if '/' in hash:
-    tmp = hash[::-1]
-    hash = tmp[:tmp.find('/')][::-1]
-
-async def random_sprite():
+async def random_sprite(rom_path,hash,heart_speed,heart_color,sprite,background_music,quickswap,menu_speed,msu_choice, auto_start):
     base_rom = await pyz3r.rom.read(rom_path)
     seed = await pyz3r.alttpr(hash_id=hash)
 
@@ -54,17 +32,78 @@ async def random_sprite():
         quickswap=quickswap,
         menu_speed=menu_speed
     )
-    
-    if not os.path.isdir('output'):
-        os.mkdir('output')
-    
-    await pyz3r.rom.write(patched_rom, 'output/' + hash + '.sfc')
 
-    if os.name == 'nt':
-        print('File written to ' + os.getcwd() + '\\output\\' + hash + '.sfc')
+    if msu_choice:
+        if msu_choice < 0:
+            msu_choice = rd.randint(1,len(msupacks)+1)
+
+        outpath = settings['msupath'] + '/' + msupacks[msu_choice-1]
+        for f in os.listdir(outpath)[::-1]:
+            if f[-4:] == '.msu':
+                fname = (f[-5::-1])[::-1]
+                break
+        await pyz3r.rom.write(patched_rom, outpath + '/' + fname + '.sfc')
+        print('File written to ' + outpath + '/' + fname + '.sfc')
+        # i, j = 0, 0
+        # while j < len(outpath):
+        #     while j < len(outpath) and outpath[j] != '/':
+        #         j += 1
+        #     print('cd ' + '\"' + outpath[i:j] + '\"', os.getcwd())
+        #     os.system('cd ' + '\"' + outpath[i:j] + '\"')
+        #     i = j+1
+        #     j += 1
+        if auto_start:
+            os.chdir(outpath)
+            os.system(fname + '.sfc')
+
     else:
-        print('File written to ' + os.getcwd() + '/output/' + hash + '.sfc')
+        await pyz3r.rom.write(patched_rom, 'seed.sfc')
+        if os.name == 'nt':
+            print('File written to ' + os.getcwd() + '\\seed.sfc')
+        else:
+            print('File written to ' + os.getcwd() + '/seed.sfc')
+
+        if auto_start:
+            os.system('seed.sfc')
 
 if __name__ == '__main__':
+
+    # Settings (loaded from config.yaml)
+
+    settings = load(open('config.yaml'), Loader=Loader)
+    settings['heart_speed']['off'] = settings['heart_speed'].pop(False)
+
+    if os.name == 'nt':
+        rom_path = os.getcwd() + '\\' + settings['rompath']
+        msu_path = os.getcwd() + '\\' + settings['msupath']
+    else:
+        rom_path = os.getcwd() + '/' + settings['rompath']
+        msu_path = os.getcwd() + '/' + settings['msupath']
+
+    auto_start = int(settings['auto_start'])
+
+    heart_speed = choose_setting(settings['heart_speed'])
+    menu_speed = choose_setting(settings['menu_speed'])
+    sprite = choose_setting(settings['sprite'])
+    heart_color = choose_setting(settings['heart_color'])
+    background_music = choose_setting(settings['background_music'])
+    quickswap = choose_setting(settings['quickswap'])
+
+    hash = input('Please paste seed URL or hash here : ')
+    if '/' in hash:
+        tmp = hash[::-1]
+        hash = tmp[:tmp.find('/')][::-1]
+
+    msu_choice = 0
+    if settings['msupath']:
+        print('Available MSU Packs :')
+        print('-1 : Random')
+        print('0 : Default')
+        msupacks = os.listdir(msu_path)
+        for i in range(len(msupacks)):
+            print(str(i+1) + ' : ' + msupacks[i])
+        msu_choice = int(input('Please choose a MSU Pack : '))
+
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(random_sprite())
+    loop.run_until_complete(random_sprite(rom_path,hash,heart_speed,heart_color,sprite,background_music,quickswap,menu_speed,msu_choice,auto_start))
+
